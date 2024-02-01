9d222d521b93d770e2fe71db914d759dd2f17d90
shahidrashid1942 <sr14201721@gmail.com>
2024-02-01 14:04:34 +0530

    fix port

--- app.js
+++ app.js
@@ -30,6 +30,6 @@ app.post("/weather", function (req, res) {
     });
 });
 
-app.listen(3000, function () {
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("server is up and running.")
 });
